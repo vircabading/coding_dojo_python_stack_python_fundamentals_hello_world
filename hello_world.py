b87a678d43a161ddc88973040ee1be0b5ad0361b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-25 12:36:26 -0700

    initialize

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+# //////////////////////////////////////////////////////////
+# Subj: Coding Dojo > Python Stack > Python > Fundamentals
+# By:   Virgilio D. Cabading Jr.
+# //////////////////////////////////////////////////////////
+
+# //////////////////////////////////////////////////////////
+print("1. TASK: print 'Hello World'")
+print ("Hello World")
+
+print( your code here )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print( your code here )	# with a comma
+print( your code here )	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print( your code here )	# with a comma
+print( your code here )	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( your code here ) # with .format()
+print( your code here ) # with an f string
+
+
